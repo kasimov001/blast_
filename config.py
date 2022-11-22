b644901cfe696d836c6991ea09fdf96c0e50cf90
kasimov001 <116807259+kasimov001@users.noreply.github.com>
2022-11-22 18:01:03 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from aiogram import Bot, Dispatcher
 
-token = '5604126911:AAFqOR7X7JYD2Wx8Ltli39-_xtgW4_sOeR8'
+token = "5738222542:AAFmLMIZmV31aOYolysYtRMXJIR0iCNkdF4"
 bot = Bot(token=token)
 dp = Dispatcher(bot)
